ae5b06eb34ae3dd9f69673bb25dbc3208beebb9b
UBC Student <jerico@students.cs.ubc.ca>
2025-11-27 17:43:14 -0800

    Implemented routes for getting Hikes, AppUsers, and Preferences, inserting AppUsers and Preferences, updating AppUsers, and deleting AppUsers

--- project/appController.js
+++ project/appController.js
@@ -15,23 +15,73 @@ const express = require('express');
      }
  });
 
- router.get('/demotable', async (req, res) => {
-     const tableContent = await appService.fetchDemotableFromDb();
-     res.json({data: tableContent});
+//  router.get('/demotable', async (req, res) => {
+//      const tableContent = await appService.fetchDemotableFromDb();
+//      res.json({data: tableContent});
+//  });
+
+//  router.post("/initiate-demotable", async (req, res) => {
+//      const initiateResult = await appService.initiateDemotable();
+//      if (initiateResult) {
+//          res.json({ success: true });
+//      } else {
+//          res.status(500).json({ success: false });
+//      }
+//  });
+
+//  router.post("/insert-demotable", async (req, res) => {
+//      const { id, name } = req.body;
+//      const insertResult = await appService.insertDemotable(id, name);
+//      if (insertResult) {
+//          res.json({ success: true });
+//      } else {
+//          res.status(500).json({ success: false });
+//      }
+//  });
+
+//  router.post("/update-name-demotable", async (req, res) => {
+//      const { oldName, newName } = req.body;
+//      const updateResult = await appService.updateNameDemotable(oldName, newName);
+//      if (updateResult) {
+//          res.json({ success: true });
+//      } else {
+//          res.status(500).json({ success: false });
+//      }
+//  });
+
+//  router.get('/count-demotable', async (req, res) => {
+//      const tableCount = await appService.countDemotable();
+//      if (tableCount >= 0) {
+//          res.json({ 
+//              success: true,  
+//              count: tableCount
+//          });
+//      } else {
+//          res.status(500).json({ 
+//              success: false, 
+//              count: tableCount
+//          });
+//      }
+//  });
+
+ router.get('/appusers', async (req, res) => {
+     const users = await appService.fetchAppUsersFromDb();
+     res.json({data: users});
  });
 
- router.post("/initiate-demotable", async (req, res) => {
-     const initiateResult = await appService.initiateDemotable();
-     if (initiateResult) {
-         res.json({ success: true });
-     } else {
-         res.status(500).json({ success: false });
-     }
+ router.get('/hikes', async (req, res) => {
+     const users = await appService.selectHike();
+     res.json({data: users});
  });
 
- router.post("/insert-demotable", async (req, res) => {
-     const { id, name } = req.body;
-     const insertResult = await appService.insertDemotable(id, name);
+router.get('/preferences', async (req, res) => {
+     const users = await appService.fetchPreferencesFromDb();
+     res.json({data: users});
+ });
+
+  router.post("/insert-appuser", async (req, res) => {
+     const { uid, name, pid, email, pnum } = req.body;
+     const insertResult = await appService.insertAppUser(uid, name, pid, email, pnum);
      if (insertResult) {
          res.json({ success: true });
      } else {
@@ -39,9 +89,19 @@ const express = require('express');
      }
  });
 
- router.post("/update-name-demotable", async (req, res) => {
-     const { oldName, newName } = req.body;
-     const updateResult = await appService.updateNameDemotable(oldName, newName);
+ router.post("/insert-preference", async (req, res) => {
+     const { pid, dist, dur, elev, diff } = req.body;
+     const insertResult = await appService.insertPreference(pid, dist, dur, elev, diff);
+     if (insertResult) {
+         res.json({ success: true });
+     } else {
+         res.status(500).json({ success: false });
+     }
+ });
+
+  router.post("/update-appuser", async (req, res) => {
+     const { uid, newName, email, pnum } = req.body;
+     const updateResult = await appService.updateAppUser(uid, newName, email, pnum);
      if (updateResult) {
          res.json({ success: true });
      } else {
@@ -49,20 +109,19 @@ const express = require('express');
      }
  });
 
- router.get('/count-demotable', async (req, res) => {
-     const tableCount = await appService.countDemotable();
-     if (tableCount >= 0) {
-         res.json({ 
-             success: true,  
-             count: tableCount
-         });
-     } else {
-         res.status(500).json({ 
-             success: false, 
-             count: tableCount
-         });
-     }
- });
+ router.delete('/appusers/:uid', async (req, res) => {
+  const { uid } = req.params;
 
+  try {
+    const deleted = await appService.deleteAppUser(Number(uid));
+    if (deleted) {
+      res.json({ success: true });
+    } else {
+      res.status(404).json({ success: false, message: 'User not found' });
+    }
+  } catch (err) {
+    res.status(500).json({ success: false, message: 'Failed to delete user' });
+  }
+});
 
  module.exports = router;
